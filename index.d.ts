0826bb0290bef4694b54a04f42dc644268ba5169
Forbes Lindesay <forbes@lindesay.co.uk>
2020-03-19 18:34:48 +0000

    fix: typescript definition (#24)

--- index.d.ts
+++ index.d.ts
@@ -13,15 +13,15 @@ export interface Options {
   cutoff?: number;
 }
 
-declare function parse(str: string, options?: Options): string;
+declare function parse(str: string, options?: Options): string | null;
 export default parse;
 
-export function parseNearbyDays(str: string, now: Date): string;
-export function parseLastThisNext(string: string, now: Date): string;
-export function parseAgoFrom(string: string, now: Date): string;
-export function parseNumberDate(string: string, usa: boolean): string;
-export function parseNumberDateShortYear(string: string, usa: boolean, cutoff: number): string;
-export function parseNumberDateNoYear(string: string, usa: boolean, now: Date): string;
-export function parseWordyDate(string: string, now: Date, cutoff: number): string;
-export function monthFromName(month: string): number;
-export function date(year: number, month: number, day: number): string;
+export function parseNearbyDays(str: string, now: Date): string | null;
+export function parseLastThisNext(string: string, now: Date): string | null;
+export function parseAgoFrom(string: string, now: Date): string | null;
+export function parseNumberDate(string: string, usa: boolean): string | null;
+export function parseNumberDateShortYear(string: string, usa: boolean, cutoff: number): string | null;
+export function parseNumberDateNoYear(string: string, usa: boolean, now: Date): string | null;
+export function parseWordyDate(string: string, now: Date, cutoff: number): string | null;
+export function monthFromName(month: string): number | null;
+export function date(year: number, month: number, day: number): string | null;
