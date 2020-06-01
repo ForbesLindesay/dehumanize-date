8ec0cfc944084e3259663d63175acdc5aeb54095
backmeupplz <backmeupplz@gmail.com>
2020-06-01 12:53:15 -0700

    add italian

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
-'use strict';
+'use strict'
 
-var isLeapYear = require('is-leap-year');
+var isLeapYear = require('is-leap-year')
 
-var MONTH_NAMES = ["january", "february", "march",
-                   "april",   "may",      "june",
-                   "july",    "august",   "september",
-                   "october", "november", "december"];
-var MONTH_NAMES_RU = ["январь", "февраль", "март",
-                   "апрель",   "май",      "июнь",
-                   "июль",    "август",   "сентябрь",
-                   "октябрь", "ноябрь", "декабрь"];
+var MONTH_NAMES = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+]
+var MONTH_NAMES_RU = [
+  'январь',
+  'февраль',
+  'март',
+  'апрель',
+  'май',
+  'июнь',
+  'июль',
+  'август',
+  'сентябрь',
+  'октябрь',
+  'ноябрь',
+  'декабрь',
+]
+var MONTH_NAMES_IT = [
+  'gennaio',
+  'febbraio',
+  'marzo',
+  'aprile',
+  'maggio',
+  'giugno',
+  'luglio',
+  'agosto',
+  'settembre',
+  'ottobre',
+  'novembre',
+  'dicembre',
+]
 
-var DAY_NAMES = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-var DAY_NAMES_RU = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+var DAY_NAMES = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+]
+var DAY_NAMES_RU = [
+  'воскресенье',
+  'понедельник',
+  'вторник',
+  'среда',
+  'четверг',
+  'пятница',
+  'суббота',
+]
+var DAY_NAMES_IT = [
+  'domenica',
+  'lunedì',
+  'martedì',
+  'mercoledì',
+  'giovedì',
+  'venerdì',
+  'sabato',
+]
 
 var DAYS_IN_MONTH = [
   31,
@@ -26,220 +85,278 @@ var DAYS_IN_MONTH = [
   30,
   31,
   30,
-  31
-];
+  31,
+]
 
 exports = module.exports = function parse(str, options) {
   if (typeof options !== 'object') {
-    options = {usa: options};
+    options = { usa: options }
   }
-  options = options || {};
-  options.usa = options.usa !== undefined ? options.usa : false;
-  options.now = options.now !== undefined  ? options.now : new Date();
-  options.cutoff = options.cutoff !== undefined ? options.cutoff : 80;
+  options = options || {}
+  options.usa = options.usa !== undefined ? options.usa : false
+  options.now = options.now !== undefined ? options.now : new Date()
+  options.cutoff = options.cutoff !== undefined ? options.cutoff : 80
 
-  str = str.trim().toLowerCase();
+  str = str.trim().toLowerCase()
 
-  return parseNearbyDays(str, options.now) ||
-         parseLastThisNext(str, options.now) ||
-         parseAgoFrom(str, options.now) ||
-         parseNumberDate(str, options.usa) ||
-         parseNumberDateShortYear(str, options.usa, options.cutoff) ||
-         parseNumberDateNoYear(str, options.usa, options.now) ||
-         parseWordyDate(str, options.now, options.cutoff) ||
-         parseIso8601Date(str);
-};
-exports.default = module.exports;
+  return (
+    parseNearbyDays(str, options.now) ||
+    parseLastThisNext(str, options.now) ||
+    parseAgoFrom(str, options.now) ||
+    parseNumberDate(str, options.usa) ||
+    parseNumberDateShortYear(str, options.usa, options.cutoff) ||
+    parseNumberDateNoYear(str, options.usa, options.now) ||
+    parseWordyDate(str, options.now, options.cutoff) ||
+    parseIso8601Date(str)
+  )
+}
+exports.default = module.exports
 
-var NUMBER              = /^[0-9]+$/;
-var NUMBER_WITH_ORDINAL = /^([0-9]+)(st|nd|rd|th)?$/;
-var NUMBER_DATE         = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{4})$/;
-var NUMBER_DATE_USA     = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{4})$/;
-var NUMBER_DATE_SHORT_YEAR         = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{2})$/;
-var NUMBER_DATE_SHORT_YEAR_USA     = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{2})$/;
-var NUMBER_DATE_NO_YEAR   = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])$/;
-var NUMBER_DATE_NO_YEAR_USA     = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])$/;
+var NUMBER = /^[0-9]+$/
+var NUMBER_WITH_ORDINAL = /^([0-9]+)(st|nd|rd|th)?$/
+var NUMBER_DATE = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{4})$/
+var NUMBER_DATE_USA = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{4})$/
+var NUMBER_DATE_SHORT_YEAR = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{2})$/
+var NUMBER_DATE_SHORT_YEAR_USA = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+([0-9]{2})$/
+var NUMBER_DATE_NO_YEAR = /^(3[0-1]|[1-2][0-9]|0?[1-9])[,\|\\\/\-\. ]+(1[0-2]|0?[1-9])$/
+var NUMBER_DATE_NO_YEAR_USA = /^(1[0-2]|0?[1-9])[,\|\\\/\-\. ]+(3[0-1]|[1-2][0-9]|0?[1-9])$/
 
-
-var ISO_8601_DATE       = /^([0-9]{4})[-/\\]?(1[0-2]|0?[1-9])[-/\\]?(3[0-1]|[1-2][0-9]|0?[1-9])$/;
+var ISO_8601_DATE = /^([0-9]{4})[-/\\]?(1[0-2]|0?[1-9])[-/\\]?(3[0-1]|[1-2][0-9]|0?[1-9])$/
 
 function addDays(now, numberOfDays) {
-  var result = new Date(now * 1 + numberOfDays * 60 * 60 * 24 * 1000);
-  return date(result.getFullYear(), result.getMonth(), result.getDate());
+  var result = new Date(now * 1 + numberOfDays * 60 * 60 * 24 * 1000)
+  return date(result.getFullYear(), result.getMonth(), result.getDate())
 }
 
-exports.parseNearbyDays = parseNearbyDays;
+exports.parseNearbyDays = parseNearbyDays
 function parseNearbyDays(string, now) {
-  if (string == 'today' || string == 'сегодня') {
-    return date(now.getFullYear(), now.getMonth(), now.getDate());;
-  } else if (string == 'yesterday' || string == 'вчера') {
-    return addDays(now, -1);
-  } else if (string == 'tomorrow' || string == 'завтра') {
-    return addDays(now, +1);
+  if (string == 'today' || string == 'сегодня' || string == 'oggi') {
+    return date(now.getFullYear(), now.getMonth(), now.getDate())
+  } else if (string == 'yesterday' || string == 'вчера' || string == 'ieri') {
+    return addDays(now, -1)
+  } else if (string == 'tomorrow' || string == 'завтра' || string == 'domani') {
+    return addDays(now, +1)
   } else {
-    return null;
+    return null
   }
 }
 
-exports.parseLastThisNext = parseLastThisNext;
+exports.parseLastThisNext = parseLastThisNext
 function parseLastThisNext(string, now) {
-  var tokens = string.split(/[,\s]+/);
-  if (['last', 'прошлый', 'прошлая', 'прошлое', 'this', 'этот', 'эта', 'это', 'next', 'следующий', 'следующая', 'следующее'].indexOf(tokens[0]) >= 0 &&
-      tokens.length === 2 ) {
-    var dayAbbreviations = DAY_NAMES.map(function (name) { return name.substr(0, tokens[1].length); });
-    var dayAbbreviationsRu = DAY_NAMES_RU.map(function (name) { return name.substr(0, tokens[1].length); });
-    var dayIndex = dayAbbreviations.indexOf(tokens[1]);
+  var tokens = string.split(/[,\s]+/)
+  if (
+    [
+      'last',
+      'прошлый',
+      'прошлая',
+      'прошлое',
+      'ultimo',
+      'ultima',
+      'this',
+      'этот',
+      'эта',
+      'это',
+      'questo',
+      'questa',
+      'next',
+      'следующий',
+      'следующая',
+      'следующее',
+      'scorso',
+      'scorsa',
+    ].indexOf(tokens[0]) >= 0 &&
+    tokens.length === 2
+  ) {
+    var dayAbbreviations = DAY_NAMES.map(function (name) {
+      return name.substr(0, tokens[1].length)
+    })
+    var dayAbbreviationsRu = DAY_NAMES_RU.map(function (name) {
+      return name.substr(0, tokens[1].length)
+    })
+    var dayAbbreviationsIt = DAY_NAMES_IT.map(function (name) {
+      return name.substr(0, tokens[1].length)
+    })
+    var dayIndex = dayAbbreviations.indexOf(tokens[1])
     if (dayIndex === -1) {
-      dayIndex = dayAbbreviationsRu.indexOf(tokens[1]);
+      dayIndex = dayAbbreviationsRu.indexOf(tokens[1])
     }
-    if (dayIndex !== -1 &&
-        dayAbbreviations.indexOf(tokens[1], dayIndex + 1) === -1) {
-      var dayDiff = dayIndex - now.getDay();
-      if (dayDiff < 0) dayDiff += 7;
-      if (tokens[0] === 'last') return addDays(now, dayDiff - 7);
-      if (tokens[0] === 'прошлый') return addDays(now, dayDiff - 7);
-      if (tokens[0] === 'прошлая') return addDays(now, dayDiff - 7);
-      if (tokens[0] === 'прошлое') return addDays(now, dayDiff - 7);
-      if (tokens[0] === 'this') return addDays(now, dayDiff);
-      if (tokens[0] === 'этот') return addDays(now, dayDiff);
-      if (tokens[0] === 'эта') return addDays(now, dayDiff);
-      if (tokens[0] === 'это') return addDays(now, dayDiff);
-      if (tokens[0] === 'next') return addDays(now, dayDiff + 7);
-      if (tokens[0] === 'следующий') return addDays(now, dayDiff + 7);
-      if (tokens[0] === 'следующая') return addDays(now, dayDiff + 7);
-      if (tokens[0] === 'следующее') return addDays(now, dayDiff + 7);
+    if (dayIndex === -1) {
+      dayIndex = dayAbbreviationsIt.indexOf(tokens[1])
     }
-    return null;
-  } else {
-    return null;
-  }
-}
-
-exports.parseAgoFrom = parseAgoFrom;
-function parseAgoFrom(string, now) {
-  var tokens = string.split(/[,\s]+/);
-  if (['day', 'days', 'week', 'weeks'].indexOf(tokens[1]) >= 0 &&
-      tokens[0].match(NUMBER) &&
-      ['ago', 'from'].indexOf(tokens[2]) >= 0
+    if (
+      dayIndex !== -1 &&
+      dayAbbreviations.indexOf(tokens[1], dayIndex + 1) === -1
     ) {
-      if (tokens[2] === 'ago') {
-        if (['day', 'days'].indexOf(tokens[1]) >= 0) return addDays(now, tokens[0] * -1);
-        if (['week', 'weeks'].indexOf(tokens[1]) >= 0) return addDays(now, (tokens[0] * 7) * -1);
-      }
-      if (tokens[2] === 'from') {
-        if (['day', 'days'].indexOf(tokens[1]) >= 0) return addDays(now, tokens[0]);
-        if (['week', 'weeks'].indexOf(tokens[1]) >= 0) return addDays(now, (tokens[0] * 7));
-      }
-      return null;
-    } else {
-    return null;
+      var dayDiff = dayIndex - now.getDay()
+      if (dayDiff < 0) dayDiff += 7
+      if (tokens[0] === 'last') return addDays(now, dayDiff - 7)
+      if (tokens[0] === 'прошлый') return addDays(now, dayDiff - 7)
+      if (tokens[0] === 'прошлая') return addDays(now, dayDiff - 7)
+      if (tokens[0] === 'прошлое') return addDays(now, dayDiff - 7)
+      if (tokens[0] === 'this') return addDays(now, dayDiff)
+      if (tokens[0] === 'этот') return addDays(now, dayDiff)
+      if (tokens[0] === 'эта') return addDays(now, dayDiff)
+      if (tokens[0] === 'это') return addDays(now, dayDiff)
+      if (tokens[0] === 'next') return addDays(now, dayDiff + 7)
+      if (tokens[0] === 'следующий') return addDays(now, dayDiff + 7)
+      if (tokens[0] === 'следующая') return addDays(now, dayDiff + 7)
+      if (tokens[0] === 'следующее') return addDays(now, dayDiff + 7)
+    }
+    return null
+  } else {
+    return null
   }
 }
 
-exports.parseNumberDate = parseNumberDate;
+exports.parseAgoFrom = parseAgoFrom
+function parseAgoFrom(string, now) {
+  var tokens = string.split(/[,\s]+/)
+  if (
+    ['day', 'days', 'week', 'weeks'].indexOf(tokens[1]) >= 0 &&
+    tokens[0].match(NUMBER) &&
+    ['ago', 'from'].indexOf(tokens[2]) >= 0
+  ) {
+    if (tokens[2] === 'ago') {
+      if (['day', 'days'].indexOf(tokens[1]) >= 0)
+        return addDays(now, tokens[0] * -1)
+      if (['week', 'weeks'].indexOf(tokens[1]) >= 0)
+        return addDays(now, tokens[0] * 7 * -1)
+    }
+    if (tokens[2] === 'from') {
+      if (['day', 'days'].indexOf(tokens[1]) >= 0)
+        return addDays(now, tokens[0])
+      if (['week', 'weeks'].indexOf(tokens[1]) >= 0)
+        return addDays(now, tokens[0] * 7)
+    }
+    return null
+  } else {
+    return null
+  }
+}
+
+exports.parseNumberDate = parseNumberDate
 function parseNumberDate(str, usa) {
-  var match = usa ? NUMBER_DATE_USA.exec(str) : NUMBER_DATE.exec(str);
+  var match = usa ? NUMBER_DATE_USA.exec(str) : NUMBER_DATE.exec(str)
   if (match) {
-    return usa ? date(+match[3], match[1] - 1, +match[2]) : date(+match[3], match[2] - 1, +match[1]);
+    return usa
+      ? date(+match[3], match[1] - 1, +match[2])
+      : date(+match[3], match[2] - 1, +match[1])
   } else {
-    return null;
+    return null
   }
 }
 
-exports.parseNumberDateShortYear = parseNumberDateShortYear;
+exports.parseNumberDateShortYear = parseNumberDateShortYear
 function parseNumberDateShortYear(str, usa, cutoff) {
-  var match = usa ? NUMBER_DATE_SHORT_YEAR_USA.exec(str) : NUMBER_DATE_SHORT_YEAR.exec(str);
+  var match = usa
+    ? NUMBER_DATE_SHORT_YEAR_USA.exec(str)
+    : NUMBER_DATE_SHORT_YEAR.exec(str)
   if (match) {
-    var year = (+match[3]);
-    if (year > cutoff) year += 1900;
-    else year += 2000;
-    return usa ? date(year, match[1] - 1, +match[2]) : date(year, match[2] - 1, +match[1]);
+    var year = +match[3]
+    if (year > cutoff) year += 1900
+    else year += 2000
+    return usa
+      ? date(year, match[1] - 1, +match[2])
+      : date(year, match[2] - 1, +match[1])
   } else {
-    return null;
+    return null
   }
 }
 
-exports.parseNumberDateNoYear = parseNumberDateNoYear;
+exports.parseNumberDateNoYear = parseNumberDateNoYear
 function parseNumberDateNoYear(str, usa, today) {
-  var match = usa ? NUMBER_DATE_NO_YEAR_USA.exec(str) : NUMBER_DATE_NO_YEAR.exec(str);
+  var match = usa
+    ? NUMBER_DATE_NO_YEAR_USA.exec(str)
+    : NUMBER_DATE_NO_YEAR.exec(str)
   if (match) {
-    var year = today.getFullYear();
-    return usa ? date(year, match[1] - 1, +match[2]) : date(year, match[2] - 1, +match[1]);
+    var year = today.getFullYear()
+    return usa
+      ? date(year, match[1] - 1, +match[2])
+      : date(year, match[2] - 1, +match[1])
   } else {
-    return null;
+    return null
   }
 }
 
-exports.parseWordyDate = parseWordyDate;
+exports.parseWordyDate = parseWordyDate
 function parseWordyDate(string, today, cutoff) {
-  var tokens = string.split(/[,\s]+/);
+  var tokens = string.split(/[,\s]+/)
   if (tokens.length >= 2) {
-    var match;
-    if (match = tokens[0].match(NUMBER_WITH_ORDINAL)) {
-      return parseWordyDateParts(match[1], tokens[1], tokens[2], today, cutoff);
-    } else if (match = tokens[1].match(NUMBER_WITH_ORDINAL)) {
-      return parseWordyDateParts(match[1], tokens[0], tokens[2], today, cutoff);
+    var match
+    if ((match = tokens[0].match(NUMBER_WITH_ORDINAL))) {
+      return parseWordyDateParts(match[1], tokens[1], tokens[2], today, cutoff)
+    } else if ((match = tokens[1].match(NUMBER_WITH_ORDINAL))) {
+      return parseWordyDateParts(match[1], tokens[0], tokens[2], today, cutoff)
     } else {
-      return null;
+      return null
     }
   }
 }
 
 function parseWordyDateParts(rawDay, rawMonth, rawYear, today, cutoff) {
-  var day = +rawDay;
-  var month = monthFromName(rawMonth);
-  var year;
+  var day = +rawDay
+  var month = monthFromName(rawMonth)
+  var year
 
-  if (rawYear)
-    year = rawYear.match(NUMBER) ? rawYear * 1 : null;
-  else
-    year = today.getFullYear();
+  if (rawYear) year = rawYear.match(NUMBER) ? rawYear * 1 : null
+  else year = today.getFullYear()
 
-  if (year < 100 && year >= cutoff) year += 1900;
-  if (year < 100) year += 2000;
+  if (year < 100 && year >= cutoff) year += 1900
+  if (year < 100) year += 2000
 
-  if (!(day && month !== null && year))
-    return null;
+  if (!(day && month !== null && year)) return null
 
-  return date(year, month, day);
+  return date(year, month, day)
 }
 
 function parseIso8601Date(string) {
-  var match;
-  if (match = ISO_8601_DATE.exec(string)) {
-    return date(+match[1], match[2] - 1, +match[3]);
+  var match
+  if ((match = ISO_8601_DATE.exec(string))) {
+    return date(+match[1], match[2] - 1, +match[3])
   } else {
-    return null;
+    return null
   }
 }
 
-exports.monthFromName = monthFromName;
+exports.monthFromName = monthFromName
 function monthFromName(month) {
-  var monthAbbreviations = MONTH_NAMES.map(function (name) { return name.substr(0, month.length); });
-  var monthAbbreviationsRu = MONTH_NAMES_RU.map(function (name) { return name.substr(0, month.length); });
-  var monthIndex = monthAbbreviations.indexOf(month);
+  var monthAbbreviations = MONTH_NAMES.map(function (name) {
+    return name.substr(0, month.length)
+  })
+  var monthAbbreviationsRu = MONTH_NAMES_RU.map(function (name) {
+    return name.substr(0, month.length)
+  })
+  var monthAbbreviationsIt = MONTH_NAMES_IT.map(function (name) {
+    return name.substr(0, month.length)
+  })
+  var monthIndex = monthAbbreviations.indexOf(month)
   if (monthIndex === -1) {
-    var monthIndex = monthAbbreviationsRu.indexOf(month);
+    monthIndex = monthAbbreviationsRu.indexOf(month)
   }
-  if (monthIndex !== -1 &&
-      monthAbbreviations.indexOf(month, monthIndex + 1) === -1) {
-    return monthIndex;
+  if (monthIndex === -1) {
+    monthIndex = monthAbbreviationsIt.indexOf(month)
   }
-  return null;
+  if (
+    monthIndex !== -1 &&
+    monthAbbreviations.indexOf(month, monthIndex + 1) === -1
+  ) {
+    return monthIndex
+  }
+  return null
 }
 
-exports.date = date;
+exports.date = date
 function date(year, month, day) {
-  month++;
-  if (month > 12 || month < 1) return null;
-  if (day < 1) return null;
+  month++
+  if (month > 12 || month < 1) return null
+  if (day < 1) return null
   if (month === 2) {
-    if (day > (isLeapYear(year) ? 29 : 28)) return null;
+    if (day > (isLeapYear(year) ? 29 : 28)) return null
   } else if (day > DAYS_IN_MONTH[month - 1]) {
-    return null;
+    return null
   }
-  if (month < 10) month = '0' + month;
-  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month
+  if (day < 10) day = '0' + day
 
-  return year + '-' + month + '-' + day;
+  return year + '-' + month + '-' + day
 }
